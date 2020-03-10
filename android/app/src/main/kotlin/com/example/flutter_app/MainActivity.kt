ace65d1859efd1c0ebd329f767e532d9d01c30e3
freedomangelly <674919909@qq.com>
2020-03-11 07:14:06 +0800

    完成android回调

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.example.flutter_app
 
+import android.app.AlertDialog
 import android.os.Bundle
-
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+    GeneratedPluginRegistrant.registerWith(this)//这个是创建Flutter工程时MainActivity自带的。
+
+    var methodChannel=MethodChannel(flutterView,"com.example.platform_channel/dalog")
+    methodChannel.setMethodCallHandler(object : MethodCallHandler {
+      //3
+      override fun onMethodCall(methodCall: MethodCall, result: MethodChannel.Result) {
+        if ("dialog" == methodCall.method) {
+          if (methodCall.hasArgument("content")) {
+            showAlertDialog()
+            result.success("弹出成功")
+          } else {
+            result.error("error", "弹出失败", "content is null")
+          }
+        } else {
+          result.notImplemented()
+        }
+      }
+
+      private fun showAlertDialog() {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setPositiveButton("确定", null)
+        builder.setTitle("Flutter调用Android")
+        builder.show()
+      }
+    })
+
   }
 }
